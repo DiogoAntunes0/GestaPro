a59af2b20d481685df3f23e53bdf2b1cd15b4c0f
Diogo Antunes <diogoantunes892@gmail.com>
2026-09-01 21:15:54 -0300

    feat: adicionando campos para cadastrar endereço/CNPJ

--- src/main/java/com/example/CoreCommerce/repository/ProdutoRepository.java
+++ src/main/java/com/example/CoreCommerce/repository/ProdutoRepository.java
@@ -1,12 +1,14 @@
 package com.example.CoreCommerce.repository;
 
 import com.example.CoreCommerce.entity.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository <Produto, Long> {
     Produto deleteProdutosById(Long id);
-    List<Produto> findAllByOrderByNomeAsc();
+    Page<Produto> findAllByOrderByNomeAsc(Pageable pageable);
 
 }
